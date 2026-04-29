d0c0798ab817e1c70f525f1db16a69854ae432ee
Qenszu <Opalusaktv@gmail.com>
2026-04-29 13:37:55 +0200

    Quick projects to review react functionality

---
+++ front/src/service/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "7bb1a21944b66379312c42c6078e8a1c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
